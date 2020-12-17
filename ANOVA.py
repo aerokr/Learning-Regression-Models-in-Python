03a02e0893010a9f8b3c0c10ce5ddb746de331a8
aerokr <75866005+aerokr@users.noreply.github.com>
2020-12-17 17:57:35 -0600

    Update ANOVA.py

--- ANOVA.py
+++ ANOVA.py
@@ -2,7 +2,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as mp # plotting 
 import statsmodels.formula.api as sm 
-import statsmodels.stats as sms #OLS model
+import statsmodels.stats as sms #ANOVA model
 
 # Creating the dataframe for the houseing data
 Housedb = pd.read_csv('train.csv')
@@ -10,9 +10,12 @@ Housedb = pd.read_csv('train.csv')
 Housedb.head()
 
 #Gather
-Deeds = Housedb.loc[Housedb['SaleType'].isin(['WD','New'])]
+Deeds = Housedb.loc[Housedb['SaleType'].isin(['New','WD'])]
+print(Deeds.shape)
+Deeds['SaleType'].value_counts()
+Deeds['KitchenQual'].value_counts()
 print(Deeds)
 
-House_lm = sm.ols(formula="SalePrice ~ C(SaleType)",data=Deeds).fit()
+House_lm = sm.ols(formula="SalePrice ~ C(SaleType)+C(Fence)",data=Deeds).fit()
 table = sms.anova.anova_lm(House_lm,typ=1)
 print(table)
